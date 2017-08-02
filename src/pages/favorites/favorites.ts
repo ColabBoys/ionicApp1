8891ccf8cc2da67a78927e7ad045dc87fb51728d
shareDemCodez <shabari.cs@gmail.com>
2017-08-01 21:40:14 -0400

    Ionic changes and sliding feature enabling with click listenere to delete with trash icon on button..

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -9,49 +9,56 @@ import { QuotePage } from "../quote/quote";
   templateUrl: 'favorites.html',
 })
 export class FavoritesPage {
-    quotes: Quote[];
+  quotes: Quote[];
 
-    constructor (private quotesService: QuotesService, private modalCtrl: ModalController) {}
+  constructor(private quotesService: QuotesService, private modalCtrl: ModalController) { }
 
-    ionViewWillEnter() {
-      this.quotes = this.quotesService.getFavoriteQuotes();
-    }
+  ionViewWillEnter() {
+    this.quotes = this.quotesService.getFavoriteQuotes();
+  }
 
-    onViewQuote(quote: Quote) {
-      // modal create needs a page to load as an argument make sure to import it
-      // second argument you can pass any data so in this case its the quote
-      // passes the selected quote in the modal
-      const modal = this.modalCtrl.create(QuotePage, quote);
-      
-      // to present to user
-      modal.present();
+  // remove via sliding
+  onRemoveFromFavorites(quote: Quote) {
+    // remove from fav
+    this.quotesService.removeQuoteFromFavorites(quote);
 
-      //modal is just a view so can execute all the methods that you can with a view controller
-      // can actually dismiss the modal from here too
-      // on did dismiss takes a function as an argument which is executed whenever the view is dismissed -- callback function
-      modal.onDidDismiss((remove: boolean) => {
-        // if remove is true from the unfav button then remove it using the Service function
-        // then reload the favorites page full of quotes (2) methods the first one is shorter
-        if(remove)
-          {
-            this.quotesService.removeQuoteFromFavorites(quote);
-            this.quotes = this.quotesService.getFavoriteQuotes();
-            // also this..
-            // const position = this.quotes.findIndex((quoteEl: Quote) => {
-            //   return quoteEl.id == quote.id;
-            // });
-            // this.quotes.splice(position, 1);
-            
-          }
-        console.log(remove);
-      });
+    // also this.. can do the same as below and above
+    // this.quotes = this.quotesService.getFavoriteQuotes();
+    // remove and reload
+    const position = this.quotes.findIndex((quoteEl: Quote) => {
+      return quoteEl.id == quote.id;
+    });
+    this.quotes.splice(position, 1);
+  }
 
-      // // before its about to leave but right before leaving so can track the before and after
-      // didLeave does the same thing but is fired closer to the onDidDismiss but the data will still be before
-      // because these functions will not grab the data passed in the dismiss() function 
-      // both are OBSERVABLES
-      // modal.willLeave.subscribe(
-      //   (remove: boolean) => console.log(remove)
-      // );
-    }
+  onViewQuote(quote: Quote) {
+    // modal create needs a page to load as an argument make sure to import it
+    // second argument you can pass any data so in this case its the quote
+    // passes the selected quote in the modal
+    const modal = this.modalCtrl.create(QuotePage, quote);
+
+    // to present to user
+    modal.present();
+
+    //modal is just a view so can execute all the methods that you can with a view controller
+    // can actually dismiss the modal from here too
+    // on did dismiss takes a function as an argument which is executed whenever the view is dismissed -- callback function
+    modal.onDidDismiss((remove: boolean) => {
+      // if remove is true from the unfav button then remove it using the Service function
+      // then reload the favorites page full of quotes (2) methods the first one is shorter
+      if (remove) {
+        // remove from fav
+        this.onRemoveFromFavorites(quote);
+      }
+      console.log(remove);
+    });
+
+    // // before its about to leave but right before leaving so can track the before and after
+    // didLeave does the same thing but is fired closer to the onDidDismiss but the data will still be before
+    // because these functions will not grab the data passed in the dismiss() function 
+    // both are OBSERVABLES
+    // modal.willLeave.subscribe(
+    //   (remove: boolean) => console.log(remove)
+    // );
+  }
 }
